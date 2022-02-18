92f971313d443367a57a1e6d2742dfb732cb3aff
Magali Fabri <magali_90@live.nl>
2022-02-18 16:11:26 +0100

    Move products array to separate file

--- index.php
+++ index.php
@@ -13,38 +13,7 @@ session_start();
 
 // VARIABLES
 
-$products = [
-    [
-        'name' => 'Your favourite drink',
-        'price' => 2.5,
-        'type' => 'drink',
-    ],
-    [
-        'name' => 'Your least-favourite drink',
-        'price' => 0.5,
-        'type' => 'drink',
-    ],
-    [
-        'name' => 'Mediocre drink',
-        'price' => 1.5,
-        'type' => 'drink',
-    ],
-    [
-        'name' => 'Your favourite food',
-        'price' => 4.5,
-        'type' => 'food',
-    ],
-    [
-        'name' => 'Your least-favourite food',
-        'price' => 2.5,
-        'type' => 'food',
-    ],
-    [
-        'name' => 'Mediocre food',
-        'price' => 3.5,
-        'type' => 'food',
-    ],
-];
+require "./products.php";
 
 $totalValue = $_COOKIE["totalValue"] ?? 0;
 
---
+++ products.php
@@ -0,0 +1,34 @@
+<?php
+
+$products = [
+    [
+        'name' => 'Your favourite drink',
+        'price' => 2.5,
+        'type' => 'drink',
+    ],
+    [
+        'name' => 'Your least-favourite drink',
+        'price' => 0.5,
+        'type' => 'drink',
+    ],
+    [
+        'name' => 'Mediocre drink',
+        'price' => 1.5,
+        'type' => 'drink',
+    ],
+    [
+        'name' => 'Your favourite food',
+        'price' => 4.5,
+        'type' => 'food',
+    ],
+    [
+        'name' => 'Your least-favourite food',
+        'price' => 2.5,
+        'type' => 'food',
+    ],
+    [
+        'name' => 'Mediocre food',
+        'price' => 3.5,
+        'type' => 'food',
+    ],
+];
